cae3161ca47347e6f1ac4d59cff1237e06af11fe
Josvme <josvme@protonmail.com>
2025-03-30 15:16:25 +0200

    Turn on backlight and wokwi

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,10 +8,10 @@
 #![no_main]
 
 use embedded_graphics::draw_target::DrawTarget;
-use embedded_graphics::Drawable;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::{Point, Primitive, RgbColor};
 use embedded_graphics::primitives::{Circle, PrimitiveStyle, Triangle};
+use embedded_graphics::Drawable;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_hal::{clock::CpuClock, delay::Delay, entry, rmt::Rmt};
 
@@ -24,9 +24,9 @@ use esp_hal::timer::timg::TimerGroup;
 use esp_println::println;
 
 use fugit::{ExtU64, RateExtU32};
-use mipidsi::Builder;
 use mipidsi::interface::SpiInterface;
-use mipidsi::models::{ST7789};
+use mipidsi::models::ST7789;
+use mipidsi::Builder;
 
 #[entry]
 fn main() -> ! {
@@ -43,10 +43,6 @@ fn main() -> ! {
         config.cpu_clock = CpuClock::max();
         config
     });
-
-
-    // use esp_println
-    println!("hello world!");
 
     // use log
     #[cfg(feature = "log")]
@@ -67,10 +63,7 @@ fn main() -> ! {
     let rmt_buffer = smartLedBuffer!(1);
     let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, rmt_buffer);
 
-    let colors = [
-        WHITE, SILVER, GRAY, BLACK, RED, MAROON, YELLOW, OLIVE, LIME, GREEN, AQUA, TEAL, BLUE,
-        NAVY, FUCHSIA, PURPLE,
-    ];
+    let colors = [WHITE, RED, YELLOW, LIME, GREEN, AQUA, BLUE];
 
     // Define the delay struct, needed for the display driver
     let mut delay = Delay::new();
@@ -81,20 +74,28 @@ fn main() -> ! {
     let mut rst = Output::new(GpioPin::<21>, Level::Low);
     rst.set_high();
 
+    // Turn on the backlight
+    let mut back_light = Output::new(GpioPin::<22>, Level::Low);
+    back_light.set_high();
+
     // Define the SPI pins and create the SPI interface
     let sck = GpioPin::<7>;
     let miso = GpioPin::<5>;
     let mosi = GpioPin::<6>;
     let cs = GpioPin::<14>;
-    let spi = Spi::new_with_config(peripherals.SPI2, Config{
-        frequency: 80_u32.kHz(),
-        mode: SpiMode::Mode0,
-        read_bit_order: SpiBitOrder::MSBFirst,
-        write_bit_order: SpiBitOrder::MSBFirst,
-    }).with_miso(miso)
-        .with_mosi(mosi)
-        .with_sck(sck)
-        .with_cs(cs);
+    let spi = Spi::new_with_config(
+        peripherals.SPI2,
+        Config {
+            frequency: 100_u32.kHz(),
+            mode: SpiMode::Mode0,
+            read_bit_order: SpiBitOrder::MSBFirst,
+            write_bit_order: SpiBitOrder::MSBFirst,
+        },
+    )
+    .with_miso(miso)
+    .with_mosi(mosi)
+    .with_sck(sck)
+    .with_cs(cs);
 
     let cs = GpioPin::<14>;
     let cs_output = Output::new(cs, Level::High);
@@ -112,39 +113,45 @@ fn main() -> ! {
         .unwrap();
 
     // Make the display all black
-    display.clear(Rgb565::RED).unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
     // Draw a smiley face with white eyes and a red mouth
-    draw_smiley(&mut display).unwrap();
 
     // Initialize the Delay peripheral, and use it to toggle the LED state in a
     // loop.
-    let led_delay = Delay::new();
-
+    let mut i = 0;
+    let mut style = PrimitiveStyle::with_fill(Rgb565::WHITE);
     loop {
-        println!("loop!");
+        println!("{} loop!", i);
 
         for color in colors {
             let data = [color; 1];
             led.write(data).unwrap();
 
-            led_delay.delay_millis(500);
+            if i == 0 {
+                style = PrimitiveStyle::with_fill(Rgb565::GREEN);
+            } else if i == 1 {
+                style = PrimitiveStyle::with_fill(Rgb565::BLUE);
+            } else if i == 2 {
+                style = PrimitiveStyle::with_fill(Rgb565::RED);
+            }
+            i = (i + 1) % 3;
+            draw_smiley(&mut display, style).unwrap();
         }
-
-        // or using `fugit` duration
-        led_delay.delay(2.secs());
     }
 }
 
-
-fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::Error> {
+fn draw_smiley<T: DrawTarget<Color = Rgb565>>(
+    display: &mut T,
+    style: PrimitiveStyle<Rgb565>,
+) -> Result<(), T::Error> {
     // Draw the left eye as a circle located at (50, 100), with a diameter of 40, filled with white
     Circle::new(Point::new(50, 100), 40)
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .into_styled(style)
         .draw(display)?;
 
     // Draw the right eye as a circle located at (50, 200), with a diameter of 40, filled with white
     Circle::new(Point::new(50, 200), 40)
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .into_styled(style)
         .draw(display)?;
 
     // Draw an upside down red triangle to represent a smiling mouth
@@ -153,8 +160,8 @@ fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::
         Point::new(130, 200),
         Point::new(160, 170),
     )
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
-        .draw(display)?;
+    .into_styled(style)
+    .draw(display)?;
 
     // Cover the top part of the mouth with a black triangle so it looks closed instead of open
     Triangle::new(
@@ -162,8 +169,8 @@ fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::
         Point::new(130, 190),
         Point::new(150, 170),
     )
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
-        .draw(display)?;
+    .into_styled(style)
+    .draw(display)?;
 
     Ok(())
 }
