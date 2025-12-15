0feb62b096c9f05c4abc1436eefdf59f4796b6c2
Josvme <josvme@protonmail.com>
2025-12-15 20:36:42 +0100

    Add LEDs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,11 +7,14 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use smart_leds::SmartLedsWrite;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use log::info;
+use esp_hal::rmt::Rmt;
+use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal_smartled::{smart_led_buffer, SmartLedsAdapter};
+use smart_leds::colors::{GREEN, RED, WHITE, YELLOW};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -28,13 +31,19 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
+
+    let mut rmt_buffer = smart_led_buffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, &mut rmt_buffer);
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
-    }
+        for color in [WHITE, RED, YELLOW, GREEN] {
+            let data = [color; 1];
+            led.write(data).unwrap();
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
+            let delay_start = Instant::now();
+            while delay_start.elapsed() < Duration::from_millis(500) {}
+        }
+    }
 }
