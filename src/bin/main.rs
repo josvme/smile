f3dc85708c8441b020fd7f96f964b5d882c8ddb9
Josvme <josvme@protonmail.com>
2025-12-16 19:27:55 +0100

    Add SPI and LCD

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,13 +7,22 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::{DrawTarget, RgbColor};
+use embedded_hal_bus::spi::ExclusiveDevice;
 use smart_leds::SmartLedsWrite;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
 use esp_hal::main;
 use esp_hal::rmt::Rmt;
 use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::spi::master::{Config, Spi};
 use esp_hal_smartled::{smart_led_buffer, SmartLedsAdapter};
+use mipidsi::Builder;
+use mipidsi::interface::SpiInterface;
+use mipidsi::models::ST7789;
 use smart_leds::colors::{GREEN, RED, WHITE, YELLOW};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -37,11 +46,61 @@ fn main() -> ! {
     let mut rmt_buffer = smart_led_buffer!(1);
     let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, &mut rmt_buffer);
 
+    // Define the Data/Command select pin as a digital output
+    let dc = Output::new(peripherals.GPIO15, Level::Low, OutputConfig::default());
+    // Define the reset pin as digital outputs and make it high
+    let mut rst = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
+    rst.set_high();
+
+    // Turn on the backlight
+    let mut back_light = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
+    back_light.set_high();
+
+    // Define the SPI pins and create the SPI interface
+    let sck = peripherals.GPIO7;
+    let miso = peripherals.GPIO5;
+    let mosi = peripherals.GPIO6;
+    let cs = peripherals.GPIO14;
+    let spi = Spi::new(
+        peripherals.SPI2,
+        Config::default().with_frequency(Rate::from_mhz(10)),
+    ).unwrap()
+        .with_miso(miso)
+        .with_mosi(mosi)
+        .with_sck(sck);
+
+    let cs_output = Output::new(cs, Level::High, OutputConfig::default());
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs_output).unwrap();
+
+    let mut buffer = [0_u8; 512];
+
+    // Define the display interface with no chip select
+    let di = SpiInterface::new(spi_device, dc, &mut buffer);
+
+    // Define the delay struct, needed for the display driver
+    let mut delay = Delay::new();
+
+    // Define the display from the display interface and initialize it
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(rst)
+        .init(&mut delay)
+        .unwrap();
+
+    let mut i = 0;
     loop {
         for color in [WHITE, RED, YELLOW, GREEN] {
             let data = [color; 1];
             led.write(data).unwrap();
 
+            let style = match i {
+                0 => Rgb565::WHITE,
+                1 => Rgb565::RED,
+                2 => Rgb565::YELLOW,
+                _ => Rgb565::GREEN
+            };
+
+            i = (i + 1) % 4;
+            display.clear(style).unwrap();
             let delay_start = Instant::now();
             while delay_start.elapsed() < Duration::from_millis(500) {}
         }
